d1e89de665592cac6f70970ce6c5b7913062020d
sfysr <muhammetsefayasar@gmail.com>
2023-09-14 11:28:17 +0300

    initial commit

--- src/http-req.js
+++ src/http-req.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://kma-api-project-f528f5982a4a.herokuapp.com/api/dataitem",
+  baseURL: "Your API Url",
   headers: {
     "Content-type": "application/json"
   }
